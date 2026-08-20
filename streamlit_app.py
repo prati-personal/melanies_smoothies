fb424a14ba23c73bca54fabf8967eec8047e2b2b
Pratikshya Priyadarshini <pratikshya.priyadarshini@gmail.com>
2026-08-21 01:09:40 +0530

    Change output from st.write to st.text

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,4 +39,4 @@ smoothiefroot_response = requests.get(
     "https://my.smoothiefroot.com/api/fruit/watermelon"
 )
 
-st.write(smoothiefroot_response.json())
+st.text(smoothiefroot_response.json())
