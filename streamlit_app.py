f6b2db2df7a65814f1e2e78d643d9898d33187ee
Pratikshya Priyadarshini <pratikshya.priyadarshini@gmail.com>
2026-08-21 01:17:43 +0530

    Comment out text display and use dataframe instead

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,4 +39,5 @@ smoothiefroot_response = requests.get(
     "https://my.smoothiefroot.com/api/fruit/watermelon"
 )
 
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df=at.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
