7878e3f554429546a32cadde0acda6103320bd3b
Pratikshya Priyadarshini <pratikshya.priyadarshini@gmail.com>
2026-08-21 09:45:35 +0530

    Uncomment dataframe display and update API request

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,8 @@ name_on_order=st.text_input('Name on Smoothie')
 st.write('The name on your smoothie will be:' ,name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
@@ -33,7 +33,7 @@ if ingredients_list:
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen+' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
         
 
