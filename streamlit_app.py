952064469ee9d76161e21852ebcdc20d175817b5
Pratikshya Priyadarshini <pratikshya.priyadarshini@gmail.com>
2026-08-21 09:23:58 +0530

    Comment out dataframe and nutrition information display
    Comment out dataframe display and stop calls. Remove unnecessary subheader and response display for nutrition information.

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,14 +14,10 @@ st.write("""Choose the fruits you want in your custom Smoothie! """
 name_on_order=st.text_input('Name on Smoothie')
 st.write('The name on your smoothie will be:' ,name_on_order)
 
-
-
-
-
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 
 pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
@@ -33,13 +29,11 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen+' '
-        
+         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
         #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        
-        st.subheader(fruit_chosen+' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+        #st.subheader(fruit_chosen+' Nutrition Information')
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
